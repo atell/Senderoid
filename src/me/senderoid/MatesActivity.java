077338365dbbb827602e7c1fc63b0f3f03132e4a
atell.wu <atell.wu@gmail.com>
2011-02-07 13:36:15 +0800

    atell atell
    Signed-off-by: atell.wu <atell.wu@gmail.com>

---
+++ src/me/senderoid/MatesActivity.java
@@ -0,0 +1,43 @@
+package me.senderoid;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Gallery;
+import android.widget.TextView;
+import android.widget.AdapterView.OnItemClickListener;
+
+public class MatesActivity extends Activity {
+
+	protected final static String tag = "MatesActivity";
+
+	private Gallery matesGallery;
+	private TextView matesTextViewName;
+	private TextView matesTextViewEmail;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.mates);
+
+		this.matesGallery = (Gallery) findViewById(R.id.mates_gallery);
+		matesGallery.setAdapter(new MatesImageAdapter(this));
+
+		this.matesTextViewName = (TextView) findViewById(R.id.mates_name_value);
+		this.matesTextViewEmail = (TextView) findViewById(R.id.mates_email_value);
+
+		matesGallery.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				MatesImageAdapter matesImageAdapter = (MatesImageAdapter) parent
+						.getAdapter();
+				matesTextViewName
+						.setText(matesImageAdapter.getMate().getName());
+				matesTextViewEmail.setText(matesImageAdapter.getMate()
+						.getEmail());
+			}
+		});
+	}
+}
---
+++ src/me/senderoid/MatesImageAdapter.java
@@ -0,0 +1,69 @@
+package me.senderoid;
+
+import me.senderoid.model.Mate;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Gallery;
+import android.widget.ImageView;
+
+public class MatesImageAdapter extends BaseAdapter {
+
+	private Mate mate;
+
+	private int matesGalleryItemBackground;
+	private Context context;
+
+	private Integer[] matesImageIds = { R.drawable.mates_1, R.drawable.mates_2,
+			R.drawable.mates_3 };
+
+	public MatesImageAdapter(Context context) {
+		this.context = context;
+		TypedArray arrt = context
+				.obtainStyledAttributes(R.styleable.mates_gallery);
+		matesGalleryItemBackground = arrt.getResourceId(
+				R.styleable.mates_gallery_android_galleryItemBackground, 0);
+		arrt.recycle();
+
+		Mate mate = new Mate();
+		mate.setEmail(System.currentTimeMillis() + "@");
+		mate.setPwd("123");
+		mate.setName("atell");
+		this.setMate(mate);
+
+	}
+
+	public int getCount() {
+		return matesImageIds.length;
+	}
+
+	public Object getItem(int position) {
+		return position;
+	}
+
+	public long getItemId(int position) {
+		return position;
+	}
+
+	public View getView(int position, View convertView, ViewGroup parent) {
+		ImageView i = new ImageView(context);
+
+		i.setImageResource(matesImageIds[position]);
+		i.setLayoutParams(new Gallery.LayoutParams(150, 100));
+		i.setScaleType(ImageView.ScaleType.FIT_XY);
+		i.setBackgroundResource(matesGalleryItemBackground);
+
+		return i;
+	}
+
+	public Mate getMate() {
+		return mate;
+	}
+
+	public void setMate(Mate mate) {
+		this.mate = mate;
+	}
+
+}
