6635ed6adbded82265f30bc53687aff52d937155
atell.wu <atell.wu@gmail.com>
2011-02-07 16:18:23 +0800

    atell

--- src/me/senderoid/TabWidget.java
+++ src/me/senderoid/TabWidget.java
@@ -3,7 +3,9 @@ package me.senderoid;
 import android.app.TabActivity;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.widget.TabHost;
 
 public class TabWidget extends TabActivity {
@@ -18,34 +20,31 @@ public class TabWidget extends TabActivity {
 		TabHost.TabSpec spec; // Resusable TabSpec for each tab
 		Intent intent; // Reusable Intent for each tab
 
+//		tabHost.setBackgroundColor(Color.argb(150, 22, 70, 150)); 
+		
 		// Create an Intent to launch an Activity for the tab (to be reused)
 		intent = new Intent().setClass(this, MatesActivity.class);
 		String mates = this.getResources().getString(R.string.mates);
-		String matesTab = this.getResources().getString(R.string.mates_tab);
-		spec = tabHost.newTabSpec(mates).setIndicator(matesTab,
-				res.getDrawable(R.drawable.mates_tab)).setContent(intent);
+		spec = tabHost.newTabSpec(mates).setIndicator(null,
+				res.getDrawable(R.drawable.home_tab)).setContent(intent);
 		tabHost.addTab(spec);
 
 		// 团队号外
 		intent = new Intent().setClass(this, IteamerActivity2.class);
 		String news = this.getResources().getString(R.string.news);
-		String newsTab = this.getResources().getString(R.string.news_tab);
-		spec = tabHost.newTabSpec(news).setIndicator(newsTab,
-				res.getDrawable(R.drawable.mates_tab)).setContent(intent);
+		spec = tabHost.newTabSpec(news).setIndicator(null,
+				res.getDrawable(R.drawable.edit_tab)).setContent(intent);
 		tabHost.addTab(spec);
 		// 会议提醒
 		intent = new Intent().setClass(this, IteamerActivity3.class);
 		String meeting = this.getResources().getString(R.string.meeting);
-		String meetingTab = this.getResources().getString(R.string.meeting_tab);
-		spec = tabHost.newTabSpec(meeting).setIndicator(meetingTab,
-				res.getDrawable(R.drawable.mates_tab)).setContent(intent);
+		spec = tabHost.newTabSpec(meeting).setIndicator(null,
+				res.getDrawable(R.drawable.help_tab)).setContent(intent);
 		tabHost.addTab(spec);
 		// 队友身影
 		intent = new Intent().setClass(this, IteamerActivity4.class);
 		String location = this.getResources().getString(R.string.location);
-		String locationTab = this.getResources().getString(
-				R.string.location_tab);
-		spec = tabHost.newTabSpec(location).setIndicator(locationTab,
+		spec = tabHost.newTabSpec(location).setIndicator(null,
 				res.getDrawable(R.drawable.mates_tab)).setContent(intent);
 		tabHost.addTab(spec);
 
